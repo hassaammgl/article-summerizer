91df4ef85eb8c145c9cb2c0f113d75e9ef5f3773
hassaammgl <h455a4mmehtab@gmail.com>
2024-05-30 17:10:29 +0500

    ok

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,17 @@ const Hero = () => {
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center flex-col">
         <img src={logo} alt="My Logo" className="w-28 object-contain" />
-        <button className="black_btn" onClick={()=> window.open("https://github.com/hassaammgl/article-summerizer" , "_blank")}>Github</button>
+        <button
+          className="black_btn"
+          onClick={() =>
+            window.open(
+              "https://github.com/hassaammgl/article-summerizer",
+              "_blank"
+            )
+          }
+        >
+          Github
+        </button>
       </nav>
     </header>
   );
