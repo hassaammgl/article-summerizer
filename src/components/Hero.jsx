fd4c9883305c07ca55ad6f238441ee4970a29d96
hassaammgl <h455a4mmehtab@gmail.com>
2024-05-30 17:09:05 +0500

    article-summerizer

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center flex-col">
+        <img src={logo} alt="My Logo" className="w-28 object-contain" />
+        <button className="black_btn" onClick={()=> window.open("https://github.com/hassaammgl/article-summerizer" , "_blank")}>Github</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Hero;
