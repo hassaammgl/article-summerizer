33af9d2797e648f204b48189799efb15f3c09121
hassaammgl <h455a4mmehtab@gmail.com>
2024-06-01 17:06:25 +0500

    project finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="My Logo" className="w-28 object-contain" />
         <button
           className="black_btn"
@@ -17,6 +17,14 @@ const Hero = () => {
           Github
         </button>
       </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with summize, an open-source article summarizer
+        that transform lengthy articles into clear and concise summaries.
+      </h2>
     </header>
   );
 };
